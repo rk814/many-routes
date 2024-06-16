0f9dde40821cb2f53cd485ec0d3c9ef56215fe79
furczak <145776102+furczak@users.noreply.github.com>
2024-06-16 14:01:42 +0200

    start project

---
+++ src/main/java/com/codecool/kgp/KgpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.kgp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KgpBackendApplication.class, args);
+	}
+
+}
