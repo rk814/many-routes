e864c048fab2993df19b103e3754b130aa6ec4f4
furczak <arch.rafal.krawczyk@gmail.com>
2024-07-16 14:12:38 +0200

    add controller and service

---
+++ src/main/java/com/codecool/kgp/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.codecool.kgp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/got/v1/users")
+public class UserController {
+}
