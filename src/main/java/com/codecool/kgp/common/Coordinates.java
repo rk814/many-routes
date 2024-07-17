80db6c3799a9ba3b4e29513a1fed9d961fe61fd7
furczak <arch.rafal.krawczyk@gmail.com>
2024-07-17 09:11:10 +0200

    improve embeded class

--- src/main/java/com/codecool/kgp/common/Coordinates.java
+++ src/main/java/com/codecool/kgp/common/Coordinates.java
@@ -1,5 +1,7 @@
 package com.codecool.kgp.common;
 
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,8 +11,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Embeddable
+@AttributeOverride(name="latitude", column=@Column(name="coords_latitude"))
+@AttributeOverride(name="longitude", column=@Column(name="coords_longitude"))
 public class Coordinates {
 
     private double latitude;
