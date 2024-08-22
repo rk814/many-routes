ce312f823ea274c95a0ecbe18b4046f4c6448348
furczak <arch.rafal.krawczyk@gmail.com>
2024-08-22 14:19:43 +0200

    challenge http

--- src/main/java/com/codecool/kgp/service/ChallengeService.java
+++ src/main/java/com/codecool/kgp/service/ChallengeService.java
@@ -11,12 +11,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@Transactional
 @Slf4j
 public class ChallengeService {
 
