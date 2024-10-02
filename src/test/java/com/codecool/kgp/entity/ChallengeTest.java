fc9bc1aa19e8951213fb9b4af2bdd331dce86c7d
furczak <arch.rafal.krawczyk@gmail.com>
2024-10-02 14:11:20 +0200

    challenge tests

---
+++ src/test/java/com/codecool/kgp/entity/ChallengeTest.java
@@ -0,0 +1,48 @@
+package com.codecool.kgp.entity;
+
+import com.codecool.kgp.entity.enums.Status;
+import org.assertj.core.api.Assertions;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.instancio.Select.field;
+
+class ChallengeTest {
+
+    @Test
+    void addSummit_shouldAddSummitToList() {
+        //given:
+        Summit summit = Instancio.create(Summit.class);
+        Challenge challenge = new Challenge("test", "test description", Status.ACTIVE);
+
+        //when:
+        challenge.addSummit(summit);
+
+        //then:
+        Assertions.assertThat(challenge.getSummitList())
+                .isNotEmpty()
+                .hasSize(1)
+                .containsExactly(summit);
+    }
+
+    @Test
+    void removeSummit_shouldRemoveSummitFromList() {
+        //given:
+        Summit summit1 = Instancio.create(Summit.class);
+        Summit summit2 = Instancio.create(Summit.class);
+        Challenge challenge = Instancio.of(Challenge.class)
+                .set(field(Challenge::getSummitList), List.of(summit1, summit2))
+                .create();
+
+        //when:
+        challenge.removeSummit(summit2);
+
+        //then:
+        Assertions.assertThat(challenge.getSummitList())
+                .isNotEmpty()
+                .hasSize(1)
+                .containsExactly(summit1);
+    }
+}
